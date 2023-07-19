ff41a697c68d94815673f32237de17452b461df2
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-19 19:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+setInterval(setDateTime,1000);
+
+
+function setDateTime() {
+const elem = document.getElementById("timer");
+const d= new Date();
+elem.innerHTML = d.toLocaleDateString() + ", " +  d.toLocaleTimeString();
+	
+}
